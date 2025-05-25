a0b9e6900d02966d92b247f73a8d63e46df61a0b
Damien Lando <damien.lando@gmail.com>
2025-05-25 16:42:52 +0200

    Delete analytics.js

--- analytics.js
+++
@@ -1,5 +0,0 @@
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'UA-XXXXXX-X'); // Replace with your Google Analytics tracking ID
